4cfc0d0f5454ae3a5b5120b98c880afb87766528
Sawyer Jones <sjones3731@sdsu.edu>
2025-01-08 17:20:00 -0800

    added more dashboard styling

---
+++ src/components/DashboardBox.js
@@ -0,0 +1,10 @@
+export const DashboardBox = {
+    marginTop: '2vh',
+    //marginRight: '55%',
+    marginLeft: '2vh',
+    border: '3px dashed #000',
+    borderRadius: '16px',
+    paddingLeft: "12px",
+    paddingTop: '6px'
+  };
+  
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
 import Sidebar from '../components/Sidebar.jsx';
+import { DashboardBox } from '../components/DashboardBox';
 
 const Dashboard = () => {
   return (
@@ -16,14 +17,25 @@ const Dashboard = () => {
         <Box sx={{ marginLeft: '5vh' }}>  
           <Typography variant="h4">Dashboard</Typography>
         </Box>
-        <Box
+        <Box // Main container
           sx={{
             marginTop: '0.5vh',
             border: '3px solid #000',
             borderRadius: '16px',
-            minHeight: '80vh'
+            minHeight: '80vh',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(2, 1fr)', // 2 equal columns
+            //gridTemplate: 'repeat(2, 1fr)',
+            gap: 2,
+            padding: 2  
           }}>
-
+            <Box sx={DashboardBox}>
+            <Typography variant="h4">Today</Typography>
+            </Box>
+            <Box sx={DashboardBox}>
+            <Typography variant="h4">Today</Typography>
+            </Box>
+            
         </Box>
       </Box>
   );
